22c6f6809183d85d04a3a9ff0a6cf6742fa710d6
unknown <lochh@kimei.vn>
2022-08-07 15:13:42 +0700

    Update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const { src, dest, watch, task, lastRun } = require('gulp');
 const uglify = require('gulp-uglify');
 const sass = require('gulp-sass')(require('sass'));
+const minify = require('gulp-minify');
 
 function defaultJS() {
   return src('./src/assets/**/*.js', { since: lastRun(defaultJS) })
@@ -14,8 +15,26 @@ function defaultCSS() {
         .pipe(dest('./src/dist/'));
 }
 
+function buildJS() {
+    return src('./src/assets/**/*.js', { since: lastRun(defaultJS) })
+        .pipe(uglify())
+        .pipe(minify())
+        .pipe(dest('./src/dist/', { sourcemaps: true }));
+}
+function buildCSS() {
+    return src('./src/assets/**/*.scss', { since: lastRun(defaultCSS) })
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(dest('./src/dist/'));
+}
+
+// Build to wordpress
+exports.buildJS = buildJS;
+exports.buildCSS = buildCSS;
+// Build to Frontend
+exports.defaultJS = defaultJS;
+exports.defaultCSS = defaultCSS;
+
 exports.default = function() {
-    task(['defaultJS', 'defaultCSS']);
     watch('./src/assets/**/*.js', defaultJS);
     watch('./src/assets/**/*.scss', defaultCSS);
 };
