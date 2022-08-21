2898ce594b6545747be680b0a2f9c67d23d99566
vihan85 <hanthuyvi0805@gmail.com>
2022-08-21 16:29:15 +0700

    slider

---
+++ src/js/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    let imgElement = $('.slider_fill-img');
+    let imgElementLength = $('.slider_fill-img').length;
+
+    function slider() {
+        let valueTransform = 0;
+        $('.slider_fill-img').each(function (index, value) {
+            let url = $(value).attr('data-bg-image');
+            $(value).css({ "background": "url(" + url + ")", "background-size": "cover", "background-repeat": "no-repeat", "background-position": "center", "right": valueTransform + "%" });
+            valueTransform = valueTransform + 100;
+        });
+        $(".slider_full").attr('data-active', 1);
+    };
+
+    slider();
+            
+    
+    setInterval(function () {
+        var index = $(".slider_full").attr('data-active');        
+
+        var right = $($('.slider_fill-img')[Number(index)]).css('right');
+
+        $(".slider_full").css('transform', "translateX(" + right + ")");
+        index = index >= imgElementLength - 1 ? 0 : Number(index) + 1;
+        $(".slider_full").attr('data-active', index);
+    }, 2000);
+}) 
