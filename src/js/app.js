cb227702af1f3e3ef8b49dd60c7aa0003db37904
vihan85 <hanthuyvi0805@gmail.com>
2022-08-21 16:59:44 +0700

    [SOFA-7] : header nav

--- src/js/app.js
+++ src/js/app.js
@@ -6,10 +6,12 @@ $(document).ready(function () {
         let valueTransform = 0;
         $('.slider_fill-img').each(function (index, value) {
             let url = $(value).attr('data-bg-image');
-            $(value).css({ "background": "url(" + url + ")", "background-size": "cover", "background-repeat": "no-repeat", "background-position": "center", "right": valueTransform + "%" });
+            $(value).css({ "background": "url(" + url + ")", "background-size": "cover", "background-repeat": "no-repeat", "background-position": "center", "left": valueTransform + "%" });
             valueTransform = valueTransform + 100;
         });
         $(".slider_full").attr('data-active', 1);
+        $(".slider_full").css('transform', "translateX(0%)");
+
     };
 
     slider();
@@ -21,7 +23,10 @@ $(document).ready(function () {
         var right = $($('.slider_fill-img')[Number(index)]).css('right');
 
         $(".slider_full").css('transform', "translateX(" + right + ")");
+
         index = index >= imgElementLength - 1 ? 0 : Number(index) + 1;
         $(".slider_full").attr('data-active', index);
-    }, 2000);
+
+    }, 6000);
+
 }) 
