b3fea8443511111fdc1ae003f613c39b52ea1671
vihan85 <hanthuyvi0805@gmail.com>
2022-08-26 01:03:38 +0700

    [SOFA-7] header-animate

--- src/js/app.js
+++ src/js/app.js
@@ -14,16 +14,16 @@ $(document).ready(function () {
     };
 
     render('.slider_fill-img');
-    render($('.product_item-img'), 0, "content-box");
-
-    function slider() {
-        $('.slider_full .slider_fill-img:gt(0)').hide();
-        console.log($(".slider_full").attr('data-speed'))
-        setInterval(function () {
-            $(".slider_full :first-child").fadeOut().next('.slider_fill-img').fadeIn().end().appendTo('.slider_full')
-        }, $(".slider_full").attr('data-speed'))
-    }
-    slider()
+    render('.product_item-img', 0, "content-box");
+    
+    // function slider() {
+    //     $('.slider_full .slider_fill-img:gt(0)').hide();
+    //     console.log($(".slider_full").attr('data-speed'))
+    //     setInterval(function () {
+    //         $(".slider_full :first-child").fadeOut(4000).next('.slider_fill-img').fadeIn(4000).end().appendTo('.slider_full')
+    //     }, $(".slider_full").attr('data-speed'))
+    // }
+    // slider()
 
 
     // setInterval(function () {
@@ -47,6 +47,32 @@ $(document).ready(function () {
     // }, $(".slider_full").attr('data-speed'));
 
 
+    var heightMenu = $('.site-header').outerHeight();
+    $(window).scroll(function (event) {
+        var scroll = $(window).scrollTop();
+        
+    
+        if(scroll >400) {
+            $('.site-header').addClass("scroll-menu")
+            $('.header-top').css("display","none")
+            $(".scroll-menu").css("top", "-100px")
+            $('.site-header').css({"transform": "translateY(100px)", "transition": "transform 2s"})
+            
+
+        }else if(scroll <300) {
+            // $('.site-header').removeClass("scroll-menu").css("position", "absolute")
+            // if(!$('.site-header').hasClass("scroll-menu")) {
+            //     // $('.header-top').css("position", "relative")
+            //     console.log('test')
+            // }
+            
+            $('.header-top').css("display","")
+            $('.site-header').css({"top":"0","transform": "translateY(36px)", "transition": "transform 3s"})
+            // $('.site-header').css({"top": "0" , "transition": "top 0.5s"})
+
+        
+        }
+    });
 
 
 })
