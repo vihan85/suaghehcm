8e000110ad494c67aa65deebe086d2641946d4e6
vihan85 <hanthuyvi0805@gmail.com>
2022-09-26 20:53:38 +0700

    [SOFA-16] update HTML, CSS

--- src/js/app.js
+++ src/js/app.js
@@ -1,31 +1,6 @@
 
 $(document).ready(function () {
-    let imgElement = $('.slider_fill-img');
-    let imgElementLength = $('.slider_fill-img').length;
-    function render(element, height, backgroundClip) {
-        $(element).each(function (index, value) {
-            let url = $(value).attr('data-bg-image');
-            $(value).children("img").attr('src',url)
-        });
-        $(".slider_full").attr('data-active', 1);
-        $(".slider_full").attr('data-speed', 2000);
-    };
-
-    render('.product_item-img',);
-    render('.project-img');
-    render('.project-outstanding_item')
-
-    function slider() {
-        $('.slider_full .slider_fill-img:gt(0)').hide();
-        $(".slider_full").attr('data-speed')
-        setInterval(function () {
-            $(".slider_full :first-child").fadeOut().next('.slider_fill-img').fadeIn().end().appendTo('.slider_full')
-        }, $(".slider_full").attr('data-speed'))
-    }
-    slider()
-
     var heightMenu = $('.main-header').outerHeight();
-
     $(window).scroll(function (event) {
         var scroll = $(window).scrollTop();
         $(".main-header").css("top", "-" + heightMenu + "px")
@@ -48,9 +23,6 @@ $(document).ready(function () {
         $(".header-search_search").toggle("500", "linear");
 
     })
-    var pc = 1024; // PC screen size.
-    var tablet = 720; // Tablet screen size.
-    var phone = 320; // Phone screen size.
 
     // btn-menu-click
     $('.btn-menu').click(function () {
@@ -68,6 +40,7 @@ $(document).ready(function () {
     $(".menu-item").after().click(function() {
         $(".sub-menu").removeClass("show")
         $(this).children(".sub-menu").addClass("show")
+
     })
 
     //slick
