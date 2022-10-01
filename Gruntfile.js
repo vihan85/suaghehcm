14852a337cb6927a59a6ea6de46bdf6668c8d39e
huynh loc <huynhhuulocit@gmail.com>
2022-10-02 00:23:35 +0700

    Refactor js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     // Project configuration.
     grunt.initConfig({
@@ -6,18 +6,30 @@ module.exports = function(grunt) {
             "options": { "separator": ";" },
             "build": {
                 "src": [
-                    "./src/assets/js/jquery-3.6.0.js",
-                    "./src/assets/plugin/slick/slick.js",
+                    "./src/assets/js/jquery.min.js",
+                    "./src/assets/plugin/slick/slick.min.js",
                     "./src/assets/js/app.js"
                 ],
                 "dest": "./src/dist/js/script.js"
             }
-        }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        },
     });
 
     // Load required modules
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
 
     // Task definitions
-    grunt.registerTask('default', ['concat']);
+    grunt.registerTask('default', ['concat', 'uglify']);
 };
