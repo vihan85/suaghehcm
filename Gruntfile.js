9d266032e37bd854d682256c1f5aaeab49f50aed
vihan85 <hanthuyvi0805@gmail.com>
2022-10-07 21:22:25 +0700

    Refactor code

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+const sass = require('node-sass');
+
+module.exports = function (grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        concat: {
+            "options": { "separator": ";" },
+            "build": {
+                "src": [
+                    "./src/assets/js/jquery.min.js",
+                    "./src/assets/plugin/slick/slick.min.js",
+                    "./src/assets/js/app.js"
+                ],
+                "dest": "./src/dist/js/script.js"
+            }
+        },
+        watch: {
+            src: {
+                files: [
+                    './src/assets/**/*.js',
+                    './src/assets/**/*.scss'
+                ],
+                tasks: ['default'],
+              },
+        },
+        uglify: {
+            dist: {
+                // Build file for wordpress
+                files: {
+                    '../suaghehcm_wp/wp-content/themes/underscores-child/assets/dist/js/script.min.js': ['<%= concat.build.dest %>'],
+                }
+            }
+        },
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    './src/dist/css/index.css': './src/assets/scss/index.scss',
+                    '../suaghehcm_wp/wp-content/themes/underscores-child/assets/dist/css/index.min.css': './src/assets/scss/index.scss'
+                }
+            }
+        }
+    });
+
+    // Load required modules
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    // Task definitions
+    grunt.registerTask('default', ['concat', 'uglify', 'sass']);
+    grunt.registerTask('grunt-watch', ['watch']);
+};
